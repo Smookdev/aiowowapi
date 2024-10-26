0a63e86b80fd2b6de0fdfb5acbc835670e07ee70
Smookdev <69019271+Smookdev@users.noreply.github.com>
2024-10-26 04:26:08 +0200

    Update game_data.py

--- src/aiowowapi/retail/game_data.py
+++ src/aiowowapi/retail/game_data.py
@@ -42,7 +42,9 @@ class GameData:
         params["locale"] = locale,
         params["access_token"] = token
 
-        return await self.api.get_resource(hostname, endpoint, params)
+        headers = {"Authorization": f"Bearer {token}"}
+
+        return await self.api.get_resource(hostname, endpoint, params, headers=headers)
 
 # region Achievement API
 
